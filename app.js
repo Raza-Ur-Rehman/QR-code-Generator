ca31c31418162503159030bffd2c4b0c5f3ae2e6
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-05-31 01:49:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 let imgBox = document.getElementById("imgbox");
 let qrImg = document.getElementById("qrimg");
 let userInput = document.getElementById("userinput");
+let validation = document.getElementById("validation");
 
 
 function generateQR() {
     if(userInput.value.length > 0) {
-
-        imgBox.innerHTML = `<img scr="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${userInput.value}" alt="" id="qrimg">`
+        qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + userInput.value;
         imgBox.classList.add("show-img");
         
     }else{
-        
+        userInput.classList.add('error');
+        validation.innerText = 'Please Enter Something!'
+        setTimeout(()=>{
+            userInput.classList.remove('error');
+            validation.innerText = ''
+        },1000)
     }
 }
